02d1e0540d5724c6d3b3a9ad4bd12c2d2bbdb67e
wishshukorn <102361217+wishshukorn@users.noreply.github.com>
2024-08-21 19:42:37 +0700

    Add files 01

---
+++ hbd-plain.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<Windows.h>
+int main(){
+    char a;
+    for(a='A';a<='H';a++){
+        printf("%c\n",a);
+        Sleep(50);
+    }printf("HA\n");
+    Sleep(50);
+    for(a='A';a<='P';a++){
+        printf("HA%c\n",a);
+        Sleep(50);
+    }for(a='A';a<='P';a++){
+        printf("HAP%c\n",a);
+        Sleep(50);
+    }for(a='A';a<='Y';a++){
+        printf("HAPP%c\n",a);
+        Sleep(50);
+    }for(a='A';a<='B';a++){
+        printf("HAPPY %c\n",a);
+        Sleep(50);
+    }for(a='A';a<='I';a++){
+        printf("HAPPY B%c\n",a);
+        Sleep(50);
+    }for(a='A';a<='R';a++){
+        printf("HAPPY BI%c\n",a);
+        Sleep(50);
+    }for(a='A';a<='T';a++){
+        printf("HAPPY BIR%c\n",a);
+        Sleep(50);
+    }for(a='A';a<='H';a++){
+        printf("HAPPY BIRT%c\n",a);
+        Sleep(50);
+    }for(a='A';a<='D';a++){
+        printf("HAPPY BIRTH %c\n",a);
+        Sleep(50);
+    }printf("HAPPY BIRTH DA\n");
+    Sleep(50);
+    for(a='A';a<='Y';a++){
+        printf("HAPPY BIRTH DA%c\n",a);
+        Sleep(50);
+    }
+}
---
+++ hbd-test.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<Windows.h>
+int main(){
+    char a;
+    // for(a=65;a<=72;a++){
+    //     printf("%c\n",a);
+    //     Sleep(50);
+    // }
+    printf("%c%c",72,65);
+}
